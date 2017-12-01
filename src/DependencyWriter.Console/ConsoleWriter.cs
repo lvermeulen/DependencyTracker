b9e25e38d5f9ee50dcc0109d20b07394ec2be470
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-12-01 20:00:58 +0100

    add ConsoleWriter

---
+++ src/DependencyWriter.Console/ConsoleWriter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Dependency.Core;
+
+namespace DependencyWriter.Console
+{
+    public class ConsoleWriter : IDependencyWriter
+    {
+        public void Write(IEnumerable<Dependency.Core.Dependency> dependencies)
+        {
+            foreach (var dependency in dependencies)
+            {
+                System.Console.WriteLine($"Project {dependency.ProjectName} depends on {dependency.DependencyId} version {dependency.DependencyVersion} with target framework {dependency.DependencyFramework}");
+            }
+        }
+    }
+}
